42f9dee20c24d0e969c719d64856e1afc9d401ef
mierla <116915007+m1erla@users.noreply.github.com>
2023-03-13 16:34:34 +0300

    Update Kodlama.io.py

--- Kodlama.io.py
+++ Kodlama.io.py
@@ -1,10 +1,11 @@
 print("*-*  'Welcome To My Student Lists'  *-*")
 
-listStudents = ["Ahmet Korkut", "Mehmet Galip", "Tekin Korkmaz"]
+listStudents = ["Ahmet Korkut", "Mehmet Galip", "Tekin Korkmaz", "Talha Koc", "Mert Gunok"]
 print(listStudents)
 
 print("Please enter name and surname for add to the list")
 def addStudent():
+    print("\n***  'Add More Student Single Time' ***\n")
     addStudentName = input("Please enter name and surname for add in the list : ")
     listStudents.append(addStudentName)
 
@@ -12,27 +13,43 @@ addStudent()
 print("Student added to the list!")
 print(listStudents)
 
+
 def addMoreStudents():
-    list = []
-    while True:
-        addMore = input("Please enter full name for add more students  : ")
-        list.append(addMore)
-        x = input("If you want to add more student just write yes or no")
-        if x == "no":
-            break
-        listStudents.extend(list)
+    print("\n***  'Add More Students Multiple Time' ***\n")
+    multipleAdd = int(input("Please enter how many students you want to add as numbers "))
+    for i in range(multipleAdd):
+        addStudent()
 
 addMoreStudents()
 print("Student added to the list!")
 print(listStudents)
 
 def deletedStudent():
+      print("\n***  'Delete Students You Want To ' ***\n")
     deletedStudent = input("Please enter the full name of the student you want deleted! :  ")
     listStudents.remove(deletedStudent)
     
 
 deletedStudent()
 print("Student deleted from list!")
+
+def removeStudent():
+    print("\n***  'Remove Students Multiple Time' ***\n")
+    print("If you want to break this condition then just write 'B' ")
+    while True:
+        fullName = input("Please enter the student's full name : ").title().strip()
+        if fullName == "B" or fullName == "b":
+            break
+        if fullName not in listStudents:
+            print("Student not found!!! Please try again!")
+            continue
+        listStudents.remove(fullName)
+        print("Student deleted from list!")
+        if len(listStudents) == 0:
+            print("All students have been successfully deleted")
+            break
+
+removeStudent()
 
 listStudents.append("Furkan Karakus")
 print("Student added to the list!")
@@ -44,14 +61,19 @@ print("Student deleted from list!")
 
 print(listStudents)
 
+print("\n***  'Whole Students ' ***\n")
 for i in range(len(listStudents)):
     print(listStudents[i])
   
+
+print("\n***  'Student Number Finder ' ***\n")
 findStudentNumber = input("If you want to learn student number then please enter student's full name : ")
 for i in range(len(listStudents)):
     if listStudents[i] == findStudentNumber:
         print(f"Student number is {i}") 
         else:
+            print("Student not found")
           break
         
+print("\n***   'Latest Status of The List'   ***\n")
 print(listStudents)
